2f446e291416e7ad3fc23fedc93bbdfa71bd53ea
auracule007 <pogooluwa12@gmail.com>
2026-04-27 07:14:46 +0100

    update  frontend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://localhost:8000",
+      "/api": "https://playtopay-8l8z.onrender.com",
     },
   },
 });
